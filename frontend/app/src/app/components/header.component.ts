6e1f165f379093e87a6283df101715e906499581
Sierra Taylor <staylor23@jcu.edu>
2023-07-17 16:04:53 -0400

    Add tailwind and daisyui for styling

--- frontend/app/src/app/components/header.component.ts
+++ frontend/app/src/app/components/header.component.ts
@@ -4,8 +4,8 @@ import { Component } from "@angular/core";
     standalone: true,
     selector: 'app-header',
     template: `
-        <header>
-            <h1>Intro to Programming</h1>
+        <header class="p-4 border-2 border-black">
+            <h1 class="text-3xl text-primary font-mono">Intro to Programming</h1>
             <p>Sample Full-Stack Application For Class!!!</p>
         </header> 
     `
---
+++ frontend/app/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('daisyui')
+  ],
+  daisyui: {
+    themes: ['synthwave', 'cyberpunk', 'cupcake']
+  }
+}
+
