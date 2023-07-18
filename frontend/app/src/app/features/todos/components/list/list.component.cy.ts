60ed7dce243d2a6adbb891308250ee827ccf218b
Sierra Taylor <staylor23@jcu.edu>
2023-07-18 13:00:47 -0400

    Create cypress tests for list component

---
+++ frontend/app/src/app/features/todos/components/list/list.component.cy.ts
@@ -0,0 +1,73 @@
+// Behavior Driven Development - Dan North
+
+import { TodoListItemModel } from '../../models';
+import { ListComponent } from './list.component';
+
+describe(ListComponent.name, () => {
+  describe('Utopian State', () => {
+    let utopianList: TodoListItemModel[] = [
+      {
+        id: 'a',
+        description: 'Buy More Beer',
+        status: 'Later',
+      },
+      {
+        id: 'b',
+        description: 'Mow the Lawn',
+        status: 'Now',
+      },
+      {
+        id: 'c',
+        description: 'Pet The Cat',
+        status: 'Waiting',
+      },
+      { id: 'd', description: 'Fix Lighting', status: 'Completed' },
+    ];
+    beforeEach(() =>
+      cy.mount(ListComponent, {
+        componentProperties: {
+          list: utopianList,
+        },
+      })
+    );
+
+    it('Has Four Items', () => {
+      cy.get('li').should('have.length', 4);
+    });
+
+    it('The First Item is For Later', () => {
+      cy.get('li')
+        .first()
+        .find('button')
+        .should('contain.text', utopianList[0].status);
+    });
+    it('The First Item is For Later', () => {
+      cy.get('li')
+        .eq(1)
+        .find('button')
+        .should('contain.text', utopianList[1].status);
+    });
+    it('Should not display an alert with a message', () => {
+      cy.get('[data-testid="empty-list-alert"]').should('not.exist');
+    });
+  });
+  describe('Empty List', () => {
+    beforeEach(() =>
+      cy.mount(ListComponent, {
+        componentProperties: {
+          list: [],
+        },
+      })
+    );
+
+    it('Should not display the list', () => {
+      cy.get('[data-testid="shopping-list"]').should('not.exist');
+    });
+
+    it('Should display an alert with a message', () => {
+      cy.get('[data-testid="empty-list-alert"]')
+        .should('exist')
+        .should('have.text', 'No Items in Your List');
+    });
+  });
+});
